3747c099249f6f33477e8777d85251503cf0797d
man2010 <mansourbalde2010@gmail.com>
2026-09-03 14:18:07 +0200

    widget video 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import { Manrope, Playfair_Display } from 'next/font/google';
 import { AuthProvider } from '@/lib/auth-context';
 import { Navbar, Footer } from '@/components/navigation';
-import { PersistentVideo } from '@/components/persistent-video';
 
 const manrope = Manrope({ subsets: ['latin'], variable: '--font-manrope' });
 const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' });
@@ -23,7 +22,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Navbar />
           {children}
           <Footer />
-          <PersistentVideo />
         </AuthProvider>
       </body>
     </html>
